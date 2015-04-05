10336dea9ef35d6eb86c0e8bfd108162a8b110fb
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-04-05 15:36:31 +0200

    forked from boulesdb; adjusted to only serve /issue

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ from google.appengine.ext import ndb
 from time import sleep
 from github import GitHub
 
-class Player(ndb.Model):
-    name = ndb.StringProperty()
-    modified = ndb.DateTimeProperty(auto_now=True)
-
 class GitHubCredentials(ndb.Model):
     owner = ndb.StringProperty()
     repo = ndb.StringProperty()
@@ -29,7 +25,7 @@ def set_githubcredentials(owner, repo, token):
     creds.put()
 
 def get_githubcredentials():
-    query = GitHubCredentials.query().order(Player.modified)
+    query = GitHubCredentials.query()
     entries = query.fetch(10)
     if len(entries) > 1:
         # TODO send email or something...
@@ -40,57 +36,6 @@ def get_githubcredentials():
         return GitHubCredentials(owner='', repo='', token='')
 
     return entries[0]
-
-NONAME = "nobody"
-
-def player_key(name=NONAME):
-    return ndb.Key('Player', name)
-
-def getPlayers():
-    players = []
-    allplayers_query = Player.query().order(Player.name)
-    # 10000 is an arbitrary threshold
-    allplayers = allplayers_query.fetch(10000)
-    for player in allplayers:
-        players.append(player.name.encode('utf-8'))
-    return players
-
-class JSONPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        self.response.headers['Access-Control-Allow-Credentials'] = 'true'
-        self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-        self.response.headers['Access-Control-Allow-Headers'] = 'x-requested-with, x-requested-by'
-        self.response.write('["%s"]'%'", "'.join(getPlayers()))
-
-class TxtPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        self.response.write('\n'.join(getPlayers()))
-
-# add new players
-EDIT_PAGE_TEMPLATE = """
-<!DOCTYPE html>
-<html lang="de">
-<head>
-<meta charset="utf-8"
-<title>edit player names</title>
-</head>
-<body>
-<div>
-Spieler:<br>
-%s
-</div>
-<br>
-<form action="/new">Neuer Spieler:<br>
-<input placeholder="Spielername" name="playername">
-<input type="submit" value="Spieler hinzufügen">
-</form>
-</body>
-</html>
-"""
 
 def validUser(user):
 #    if user.email() == "ebriigisto@gmail.com":
@@ -104,41 +49,6 @@ def validAdmin(user):
         return True
     else:
         return False
-
-class EditPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if user:
-            if validUser(user):
-                self.response.headers['Content-Type'] = 'text/html'
-                self.response.write(EDIT_PAGE_TEMPLATE%'<br>'.join(getPlayers()))
-            else:
-                self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-                self.response.write("No write access for user %s"%user.email())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-
-class PlayerEntry(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
-        else:
-            if validUser(user):
-                playername = self.request.get('playername')
-                
-                # TODO verifications
-                playername = playername.replace('"', '')
-                
-                player = Player(key=player_key(playername))
-                player.name = playername;
-                player.put()
-                
-                query_params = {'added': playername.encode('utf-8')}
-                self.redirect('/')
-            else:
-                self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-                self.response.write("No write access for user %s"%user.email())
 
 class CredentialsUpdate(webapp2.RequestHandler):
     def get(self):
@@ -342,13 +252,6 @@ class PubKeyPage(webapp2.RequestHandler):
         self.response.write("TODO: read pubkey from database or generate a disposable one")
 
 application = webapp2.WSGIApplication([
-    ('/json', JSONPage),
-    ('/txt', TxtPage),
-    ('/text', TxtPage),
-    ('/plain', TxtPage),
-    ('/raw', TxtPage),
-    ('/', EditPage),
-    ('/new', PlayerEntry),
-    ('/issue', IssuePage),
-    ('/githubsettings', CredentialsUpdate)
+    ('/', IssuePage),
+    ('/settings', CredentialsUpdate)
 ], debug=True)
